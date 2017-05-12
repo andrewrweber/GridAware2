67bcae1774a3a92a98c138a5bfb890c8478fc88c
Andrew Weber <weber@M08.local>
2017-05-12 12:01:05 -0700

    ready to deploy to cloud function

--- cloudFunctions/pushGenInfoToRedis.js
+++ cloudFunctions/pushGenInfoToRedis.js
@@ -9,41 +9,43 @@ const Config = require('../config.json');
 
 const TOKEN = Config.wattTimeToken;
 
-const redisClient = redis.createClient({
-    url: Config.redisURI
-});
+exports.pubGenInfoToRedis = function subscribe(event, callback) {
+    const redisClient = redis.createClient({
+        url: Config.redisURI
+    });
 
-//Promisify Redis ZADD calls
-redisClient.zadd = Promise.promisify(redisClient.zadd);
+    redisClient.zadd = Promise.promisify(redisClient.zadd);
 
-let options = {
-    uri: 'https://api.watttime.org/api/v1/datapoints/?ba=CAISO&page_size=20&market=RT5M',
-    headers: {
-        Authorization: `Token ${TOKEN}`
-    }
+    let options = {
+        uri: `https://api.watttime.org/api/v1/datapoints/${Config.wattTimeQS}`,
+        headers: {
+            Authorization: `Token ${TOKEN}`
+        }
+    };
+
+    request(options)
+        .then((response) => {
+            const jsonResponse = JSON.parse(response).results;
+
+            const args = ['cagenmix', 'nx'];
+            _.forEach(jsonResponse, (item) => {
+                const score = moment(item.timestamp).unix();
+                const data = JSON.stringify(item);
+
+                args.push(score);
+                args.push(data);
+            });
+
+            return redisClient.zadd(args)
+        })
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+        .finally(() => {
+            redisClient.quit();
+            callback();
+        })    
 };
-
-request(options)
-    .then((response) => {
-        const jsonResponse = JSON.parse(response).results;
-
-        const args = ['cagenmix', 'nx'];
-        _.forEach(jsonResponse, (item) => {
-            const score = moment(item.timestamp).unix();
-            const data = JSON.stringify(item);
-
-            args.push(score);
-            args.push(data);
-        });
-
-        return redisClient.zadd(args)
-    })
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((err) => {
-        console.error(err);
-    })
-    .finally(() => {
-        redisClient.quit();
-    })    
